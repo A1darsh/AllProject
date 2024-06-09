72fd245cf0bef00e4baedaa5da86ebaf3c8591d9
adarsh <adarshmishra5432@gmail.com>
2024-06-10 01:43:36 +0530

    added maven project

---
+++ src/main/java/com/adarsh/adarsh/App.java
@@ -0,0 +1,12 @@
+package com.adarsh.adarsh;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for(int i = 0 ; i<10; i++) {
+    		System.out.println( "Hello World!" );
+    	}
+        
+    }
+}
